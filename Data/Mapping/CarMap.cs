8f5e79ef02a40e2a20517e38b6e35189839c37e7
Bruno Alves de Almeida <bbruno1907.ba@gmail.com>
2021-10-27 11:57:56 -0300

    Versão 1.0

---
+++ Data/Mapping/CarMap.cs
@@ -0,0 +1,49 @@
+﻿using Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Mapping
+{
+    public class CarMap
+    {
+        public void Configure(EntityTypeBuilder<Car> builder)
+        {
+            builder.ToTable("Cars");
+
+            builder.HasKey(prop => prop.Id);
+
+            builder.Property(prop => prop.Brand)
+                .HasColumnName("Marca")
+                .HasColumnType("varchar(100)")
+                .IsRequired();
+
+            builder.Property(prop => prop.Model)
+                .HasColumnName("Modelo")
+                .HasColumnType("varchar(100)")
+                .IsRequired();
+
+            builder.Property(prop => prop.Version)
+                .HasColumnName("Versão")
+                .HasColumnType("varchar(100)")
+                .IsRequired();
+
+            builder.Property(prop => prop.Placa)
+                .HasColumnName("Placa")
+                .HasColumnType("varchar(7)")
+                .IsRequired();
+
+            builder.Property(prop => prop.Renavam)
+                .HasColumnName("Renavam")
+                .HasColumnType("varchar(11)")
+                .IsRequired();
+
+            builder.Property(prop => prop.Chassi)
+                .HasColumnName("Chassi")
+                .HasColumnType("varchar(17)")
+                .IsRequired();
+        }
+    }
+}
---
+++ Data/Mapping/PatientMap.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Mapping
+{
+    public class PatientMap
+    {
+        public void Configure(EntityTypeBuilder<Patient> builder)
+        {
+            builder.ToTable("Patients");
+
+            builder.HasKey(prop => prop.Id);
+
+            builder.Property(prop => prop.Name)
+                .HasColumnName("Name")
+                .HasColumnType("varchar(100)")
+                .IsRequired();
+
+            builder.Property(prop => prop.CPF)
+                 .HasColumnName("CPF")
+                 .HasColumnType("varchar(11)")
+                 .IsRequired();
+
+        }
+    }
+}
