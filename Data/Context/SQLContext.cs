efd07784e70efab7ad24caaf5fb5d7f8cb99f128
Bruno Alves de Almeida <bbruno1907.ba@gmail.com>
2021-10-27 10:39:58 -0300

    Versão 1.0

---
+++ Data/Context/SQLContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Context
+{
+    public class SQLContext : DbContext
+    {
+        public SQLContext(DbContextOptions<SQLContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Car>(new CarMap().Configure);
+
+            modelBuilder.Entity<Patient>(new PatientMap().Configure);
+
+        }
+    }
+}
